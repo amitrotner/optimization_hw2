3522eb40f125feea9ace0d57414f182afd0fa1c2
amitrotner <42109793+amitrotner@users.noreply.github.com>
2020-05-11 13:15:47 +0300

    Addded Graphs to PDF

--- task3.py
+++ task3.py
@@ -8,12 +8,13 @@ H_well = scipy.io.loadmat("h.mat")['H_well']
 H_ill = scipy.io.loadmat("h.mat")['H_ill']
 
 
-def plot_graph(f_name, iter, convergence_curve):
+def plot_graph(figure_title, iter, convergence_curve, save_name):
     plt.plot(range(iter), convergence_curve)
-    plt.title(f_name)
+    plt.title(figure_title)
     plt.ylabel(r"$f(x_{k})-f^*$")
     plt.xlabel("Iteration Number")
     plt.yscale('log')
+    plt.savefig('graphs/' + save_name + '.svg', format='svg')
     plt.show()
 
 
@@ -73,7 +74,7 @@ def f_ill_hess(x):
     return 0.5 * (H + H.T)
 
 
-def gradient_decent(f, f_name, der_f, x0, alpha0, sigma, beta, epsilon):
+def gradient_decent(f, der_f, x0, alpha0, sigma, beta, epsilon, figure_title, save_name):
     d = -der_f(x0)
     x = x0
     iter = 1
@@ -91,11 +92,11 @@ def gradient_decent(f, f_name, der_f, x0, alpha0, sigma, beta, epsilon):
         d = -der_f(x)
         convergence_curve += [float(f(x))]
         iter += 1
-    plot_graph(f_name, iter, convergence_curve)
+    plot_graph(figure_title, iter, convergence_curve, save_name)
     return x
 
 
-def newton(f, f_name, der_f, hes_f, x0, alpha0, sigma, beta, epsilon):
+def newton(f, der_f, hes_f, x0, alpha0, sigma, beta, epsilon, figure_title, save_name):
     L, D, e = mcholmz.modifiedChol(hes_f(x0))
     grad = der_f(x0)
     y = scipy.linalg.solve_triangular(-L, grad, lower=True)
@@ -112,14 +113,16 @@ def newton(f, f_name, der_f, hes_f, x0, alpha0, sigma, beta, epsilon):
             alpha = beta * alpha
             F_armijo = (f(x + alpha * d) - f(x))
             F_armijo_sigma = (sigma * alpha * np.matmul(der_f(x).T, d))
+        x_k = x
         x = x + alpha * d
+        print(np.linalg.norm(x_k) / np.linalg.norm(x) ** 2)
         L, D, e = mcholmz.modifiedChol(hes_f(x))
         grad = der_f(x)
         y = scipy.linalg.solve_triangular(-L, grad, lower=True)
         d = scipy.linalg.solve_triangular(np.matmul(np.diagflat(D), L.T), y, lower=False)
         convergence_curve += [float(f(x))]
         iter += 1
-    plot_graph(f_name, iter, convergence_curve)
+    plot_graph(figure_title, iter, convergence_curve, save_name)
     return x
 
 
@@ -131,11 +134,22 @@ if __name__ == '__main__':
     beta = 0.5
     epsilon = 1e-5
 
-    gradient_decent(rosen, "The Convergence Curve of Gradient Decent \n with the Rosenbrock Function", rosen_der, x0_rosen, alpha0, sigma, beta, epsilon)
-    gradient_decent(f_well, "The Convergence Curve of Gradient Decent \n with the Quadratic Function using H_well", f_well_grad, x0_quad, alpha0, sigma, beta, epsilon)
-    gradient_decent(f_ill, "The Convergence Curve of Gradient Decent \n with the Quadratic Function using H_ill", f_ill_grad, x0_quad, alpha0, sigma, beta, epsilon)
+    gradient_decent(rosen, rosen_der, x0_rosen, alpha0, sigma, beta, epsilon,
+                    figure_title="The Convergence Curve of Gradient Decent \n with the Rosenbrock Function",
+                    save_name='rosenbrock_gradient_descent')
+    gradient_decent(f_well, f_well_grad, x0_quad, alpha0, sigma, beta, epsilon,
+                    figure_title="The Convergence Curve of Gradient Decent \n with the Quadratic Function using H_well",
+                    save_name='quad_H_well_gradient_descent')
+    gradient_decent(f_ill, f_ill_grad, x0_quad, alpha0, sigma, beta, epsilon,
+                    figure_title="The Convergence Curve of Gradient Decent \n with the Quadratic Function using H_ill",
+                    save_name='quad_H_ill_gradient_descent')
 
-    newton(rosen, "The Convergence Curve of Newton's Method \n with the Rosenbrock Function", rosen_der, rosen_hess, x0_rosen, alpha0, sigma, beta, epsilon)
-    newton(f_well, "The Convergence Curve of Newton's Method \n with the Quadratic Function using H_well", f_well_grad, f_well_hess, x0_quad, alpha0, sigma, beta, epsilon)
-    newton(f_ill, "The Convergence Curve of Newton's Method \n with the Quadratic Function using H_ill", f_ill_grad, f_ill_hess, x0_quad, alpha0, sigma, beta, epsilon)
-
+    newton(rosen, rosen_der, rosen_hess, x0_rosen, alpha0, sigma, beta, epsilon,
+           figure_title="The Convergence Curve of Newton's Method \n with the Rosenbrock Function",
+           save_name='rosenbrock_newton')
+    newton(f_well, f_well_grad, f_well_hess, x0_quad, alpha0, sigma, beta, epsilon,
+           figure_title="The Convergence Curve of Newton's Method \n with the Quadratic Function using H_well",
+           save_name='quad_H_well_newton')
+    newton(f_ill, f_ill_grad, f_ill_hess, x0_quad, alpha0, sigma, beta, epsilon,
+           figure_title="The Convergence Curve of Newton's Method \n with the Quadratic Function using H_ill",
+           save_name='quad_H_ill_newton')
